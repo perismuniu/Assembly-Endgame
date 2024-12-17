23fb7d01e4fac13b7f1d196e6b08cbaadcb13add
Peris Muniu <122822923+perismuniu@users.noreply.github.com>
2024-12-17 12:24:13 +0300

    Header section styling

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function AssemblyEndgame() {
+    return (
+        <main>
+            <header>
+                <h1>Assembly</h1>
+                <p>Guess the word in under 8 attempts to keep the </p>
+                <p>programming world safe from Assembly!</p>
+            </header>
+        </main>
+    )
+}
