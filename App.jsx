3b548b2242ab4cf91e277cb81d17900f3ebe6a50
Peris Muniu <122822923+perismuniu@users.noreply.github.com>
2024-12-17 13:07:11 +0300

    game-status section styling

--- App.jsx
+++ App.jsx
@@ -4,10 +4,13 @@ export default function AssemblyEndgame() {
     return (
         <main>
             <header>
-                <h1>Assembly</h1>
-                <p>Guess the word in under 8 attempts to keep the </p>
-                <p>programming world safe from Assembly!</p>
+                <h1>Assembly: Endgame</h1>
+                <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
             </header>
+            <section className="game-status">
+                <h2>You win!</h2>
+                <p>Well done! 🎉</p>
+            </section>
         </main>
     )
 }
